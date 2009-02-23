5b9b4e87ae520e8f27a15442581b47ce16612759
Ragnar Henriksen <ragnar@shortcut.no>
2009-02-23 21:02:50 +0100

    Updated ezgraphix plugin and rewrote app to match the API changes

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -6,9 +6,29 @@ class GraphsController < ApplicationController
     ds = Ezgraphix::Data::Dataset.new( :include_root => false )
 
     {:ruby => 1, :perl => 2, :smalltalk => 3}.each do |k,v|
-      ds << Ezgraphix::Data::Set.new( v, :label => k)
+      ds << Ezgraphix::Data::Dataset::Set.new( v, :label => k)
     end
     @graph.add_dataset( ds )
+
+    tl = Ezgraphix::Data::TrendLines.new
+    tl << Ezgraphix::Data::TrendLines::Line.new( 2.43, {:displayvalue => "target", :color => '009933'} )
+    tl << Ezgraphix::Data::TrendLines::Zone.new( 1.43, 4, {:displayvalue => "range", :color => 'FD0000'} )
+
+    @graph.trend_lines = tl
+    render :action => "index"
+  end
+
+  def ss_line
+    @graph = Ezgraphix::Graphic.new( "Line")
+
+    ds = Ezgraphix::Data::Dataset.new( :include_root => false )
+
+    {:jan => 14, :feb => 22, :line => Ezgraphix::Data::Dataset::Vline.new, :mar => 21, :apr => 17, :mai => 24}.freeze.each_pair do |k,v|
+      ds << v and next if v.is_a?( Ezgraphix::Data::Dataset::Vline )
+      ds << Ezgraphix::Data::Dataset::Set.new( v, :label => k )
+    end
+    @graph.add_dataset( ds )
+
 
     render :action => "index"
   end
@@ -20,9 +40,9 @@ class GraphsController < ApplicationController
 
     ['ruby', 'perl', 'smalltalk'].each do |v|
       ds = Ezgraphix::Data::Dataset.new( :include_root => true, :seriesName => v )
-      ds << Ezgraphix::Data::Set.new( 243 + rand(243))
-      ds << Ezgraphix::Data::Set.new( 273 + rand(273))
-      ds << Ezgraphix::Data::Set.new( 223 + rand(223))
+      ds << Ezgraphix::Data::Dataset::Set.new( 243 + rand(243))
+      ds << Ezgraphix::Data::Dataset::Set.new( 273 + rand(273))
+      ds << Ezgraphix::Data::Dataset::Set.new( 223 + rand(223))
       @graph.add_dataset( ds )
     end
 
@@ -35,7 +55,7 @@ class GraphsController < ApplicationController
     ds = Ezgraphix::Data::Dataset.new( :include_root => false )
 
     {:ruby => 1, :perl => 2, :smalltalk => 3}.each do |k,v|
-      ds << Ezgraphix::Data::Set.new( v, :label => k)
+      ds << Ezgraphix::Data::Dataset::Set.new( v, :label => k)
     end
     @graph.add_dataset( ds )
 
@@ -48,9 +68,9 @@ class GraphsController < ApplicationController
     @graph.add_categories( [2007,2008,2009])
     ['ruby', 'perl', 'smalltalk'].each do |v|
       ds = Ezgraphix::Data::Dataset.new( :include_root => true, :seriesName => v )
-      ds << Ezgraphix::Data::Set.new( 243 + rand(243))
-      ds << Ezgraphix::Data::Set.new( 273 + rand(273))
-      ds << Ezgraphix::Data::Set.new( 223 + rand(223))
+      ds << Ezgraphix::Data::Dataset::Set.new( 243 + rand(243))
+      ds << Ezgraphix::Data::Dataset::Set.new( 273 + rand(273))
+      ds << Ezgraphix::Data::Dataset::Set.new( 223 + rand(223))
       @graph.add_dataset( ds )
     end
 
@@ -76,48 +96,48 @@ class GraphsController < ApplicationController
     @dds = Ezgraphix::Data::Dataset.new( :include_root => true)
 
     @ds = Ezgraphix::Data::Dataset.new( :seriesName => "Subscribe", :include_root => true )
-    @ds << Ezgraphix::Data::Set.new( 10 )
-    @ds << Ezgraphix::Data::Set.new( 20 )
-    @ds << Ezgraphix::Data::Set.new( 30 )
-    @ds << Ezgraphix::Data::Set.new( 23 )
-    @ds << Ezgraphix::Data::Set.new( 13 )
-    @ds << Ezgraphix::Data::Set.new( 27 )
-    @ds << Ezgraphix::Data::Set.new( 30 )
+    @ds << Ezgraphix::Data::Dataset::Set.new( 10 )
+    @ds << Ezgraphix::Data::Dataset::Set.new( 20 )
+    @ds << Ezgraphix::Data::Dataset::Set.new( 30 )
+    @ds << Ezgraphix::Data::Dataset::Set.new( 23 )
+    @ds << Ezgraphix::Data::Dataset::Set.new( 13 )
+    @ds << Ezgraphix::Data::Dataset::Set.new( 27 )
+    @ds << Ezgraphix::Data::Dataset::Set.new( 30 )
     @dds << @ds
 
     @ds = Ezgraphix::Data::Dataset.new( :seriesName => "Unubscribe", :include_root => true )
-    @ds << Ezgraphix::Data::Set.new( -8 )
-    @ds << Ezgraphix::Data::Set.new( -20 )
-    @ds << Ezgraphix::Data::Set.new( -10 )
-    @ds << Ezgraphix::Data::Set.new( -3 )
-    @ds << Ezgraphix::Data::Set.new( -8 )
-    @ds << Ezgraphix::Data::Set.new( -13 )
-    @ds << Ezgraphix::Data::Set.new( -2 )
+    @ds << Ezgraphix::Data::Dataset::Set.new( -8 )
+    @ds << Ezgraphix::Data::Dataset::Set.new( -20 )
+    @ds << Ezgraphix::Data::Dataset::Set.new( -10 )
+    @ds << Ezgraphix::Data::Dataset::Set.new( -3 )
+    @ds << Ezgraphix::Data::Dataset::Set.new( -8 )
+    @ds << Ezgraphix::Data::Dataset::Set.new( -13 )
+    @ds << Ezgraphix::Data::Dataset::Set.new( -2 )
 
     @dds << @ds
     @graph.add_dataset( @dds )
 
     @ls = Ezgraphix::Data::Lineset.new( :seriesName => "Total subscribers")
-    @ls << Ezgraphix::Data::Set.new( 15 )
-    @ls << Ezgraphix::Data::Set.new( 35 )
-    @ls << Ezgraphix::Data::Set.new( 45 )
-    @ls << Ezgraphix::Data::Set.new( 33 )
-    @ls << Ezgraphix::Data::Set.new( 27 )
-    @ls << Ezgraphix::Data::Set.new( 35 )
-    @ls << Ezgraphix::Data::Set.new( 15 )
+    @ls << Ezgraphix::Data::Lineset::Set.new( 15 )
+    @ls << Ezgraphix::Data::Lineset::Set.new( 35 )
+    @ls << Ezgraphix::Data::Lineset::Set.new( 45 )
+    @ls << Ezgraphix::Data::Lineset::Set.new( 33 )
+    @ls << Ezgraphix::Data::Lineset::Set.new( 27 )
+    @ls << Ezgraphix::Data::Lineset::Set.new( 35 )
+    @ls << Ezgraphix::Data::Lineset::Set.new( 15 )
     @graph.add_lineset( @ls )
 
     @ls = Ezgraphix::Data::Lineset.new( :seriesName => "Total revenue")
-    @ls << Ezgraphix::Data::Set.new( 17 )
-    @ls << Ezgraphix::Data::Set.new( 37 )
-    @ls << Ezgraphix::Data::Set.new( 47 )
-    @ls << Ezgraphix::Data::Set.new( 35 )
-    @ls << Ezgraphix::Data::Set.new( 30 )
-    @ls << Ezgraphix::Data::Set.new( 37 )
-    @ls << Ezgraphix::Data::Set.new( 17 )
+    @ls << Ezgraphix::Data::Lineset::Set.new( 17 )
+    @ls << Ezgraphix::Data::Lineset::Set.new( 37 )
+    @ls << Ezgraphix::Data::Lineset::Set.new( 47 )
+    @ls << Ezgraphix::Data::Lineset::Set.new( 35 )
+    @ls << Ezgraphix::Data::Lineset::Set.new( 30 )
+    @ls << Ezgraphix::Data::Lineset::Set.new( 37 )
+    @ls << Ezgraphix::Data::Lineset::Set.new( 17 )
     @graph.add_lineset( @ls )
 
-    @graph.add_categories( ["2003","2004","2005","2006","2007", "2008", "2009"])
+    @graph.add_categories( ["2003","2004","2005", Ezgraphix::Data::Dataset::Vline.new, "2006","2007", "2008", "2009"])
 
     render :action => "index"
   end
